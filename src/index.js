b85b1f07fa61f7a8aeae6eb80bec64ae8397c5b3
masato-bkn <37011138+masato-bkn@users.noreply.github.com>
2026-01-17 19:34:59 +0900

    version 3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
   console.log('Event:', JSON.stringify(event, null, 2));
-  console.log('version: 2');
+  console.log('version: 3');
   const response = {
     statusCode: 200,
     body: JSON.stringify({
